474904d1f7a6b78fab91490112f9dfb03c1972c3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfSpreadsheet_CustomCellEditors/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Spreadsheet;
+using DevExpress.Xpf.Editors.Settings;
+using DevExpress.Xpf.Spreadsheet;
+using System;
+
+namespace WpfSpreadsheet_CustomCellEditors
+{
+    public partial class MainWindow : DevExpress.Xpf.Ribbon.DXRibbonWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            spreadsheetControl.LoadDocument("Documents\\Document.xlsx", DocumentFormat.Xlsx);
+
+            spreadsheetControl.DocumentLoaded += spreadsheetControl1_DocumentLoaded;
+            #region #CustomCellEdit
+            spreadsheetControl.CustomCellEdit += spreadsheetControl1_CustomCellEdit;
+            // ...
+            #endregion #CustomCellEdit
+        }
+
+        private void spreadsheetControl1_DocumentLoaded(object sender, EventArgs e)
+        {
+            BindCustomEditors();
+        }
+
+        private void BindCustomEditors()
+        {
+            Worksheet worksheet = spreadsheetControl.Document.Worksheets["Sales report"];
+
+            #region #PredefinedEditors
+            // Use a date editor as the in-place editor for cells located in the "Order Date" column of the worksheet table.
+            Range dateEditRange = worksheet["Table[Order Date]"];
+            worksheet.CustomCellInplaceEditors.Add(dateEditRange, CustomCellInplaceEditorType.DateEdit);
+
+            // Use a combo box editor as the in-place editor for cells located in the "Category" column of the worksheet table.
+            // The editor's items are obtained from a cell range in the current worksheet.
+            Range comboBoxRange = worksheet["Table[Category]"];
+            worksheet.CustomCellInplaceEditors.Add(comboBoxRange, CustomCellInplaceEditorType.ComboBox, ValueObject.FromRange(worksheet["J3:J9"]));
+
+            // Use a check editor as the in-place editor for cells located in the "Discount" column of the worksheet table.
+            Range checkBoxRange = worksheet["Table[Discount]"];
+            worksheet.CustomCellInplaceEditors.Add(checkBoxRange, CustomCellInplaceEditorType.CheckBox);
+
+            // Use the custom control (SpinEdit) as the in-place editor for cells located in the "Quantity" column of the worksheet table.
+            // To provide the required editor, handle the CustomCellEdit event. 
+            Range customRange = worksheet["Table[Qty]"];
+            worksheet.CustomCellInplaceEditors.Add(customRange, CustomCellInplaceEditorType.Custom, "MySpinEdit");
+            #endregion #PredefinedEditors
+        }
+
+        #region #CustomCellEdit
+        private void spreadsheetControl1_CustomCellEdit(object sender, SpreadsheetCustomCellEditEventArgs e)
+        {
+            // Specify a type of the custom editor assigned to cells of the "Quantity" table column.
+            // To identify the custom editor, use a value of ValueObject associated with it. 
+            if (e.ValueObject.IsText && e.ValueObject.TextValue == "MySpinEdit")
+            {
+                // Create a SpinEdit in-place editor and assign it to a cell. 
+                SpinEditSettings settings = new SpinEditSettings();
+                settings.MinValue = 1;
+                settings.MaxValue = 1000;
+                settings.IsFloatValue = false;
+                e.EditSettings = settings;
+            }
+        }
+        #endregion #CustomCellEdit
+    }
+}
